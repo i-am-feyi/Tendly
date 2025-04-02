3fbe68ff84d7e96ce1238a18c48d9de3092d57f4
Feyisayo Badmus <feymus99@gmail.com>
2025-04-02 15:04:09 +0300

    fix: SVG Issues

--- sections/home/hero.tsx
+++ sections/home/hero.tsx
@@ -1,8 +1,8 @@
+import Image from "next/image";
+import { ArrowRight, Play } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import CardOutline from "@/components/ui/card-outline";
-import Sandbox from "@/public/icons/codesandbox.svg";
-import { ArrowRight, Play } from "lucide-react";
-import Image from "next/image";
 
 const Hero = () => {
   return (
@@ -13,7 +13,57 @@ const Hero = () => {
           <div className="inline-flex justify-self-center">
             <CardOutline className="p-2 rounded-full text-sm font-medium flex items-center">
               <div className="flex items-center gap-2">
-                {/* <Sandbox /> */}
+                {/* SVG CodeSandBox */}
+                <svg
+                  width="17"
+                  height="16"
+                  viewBox="0 0 17 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.9 10.4V5.60002C13.8998 5.38959 13.8442 5.18291 13.7389 5.00072C13.6336 4.81853 13.4822 4.66724 13.3 4.56202L9.09998 2.16202C8.91755 2.0567 8.71062 2.00125 8.49998 2.00125C8.28933 2.00125 8.0824 2.0567 7.89998 2.16202L3.69998 4.56202C3.51773 4.66724 3.36636 4.81853 3.26105 5.00072C3.15574 5.18291 3.10019 5.38959 3.09998 5.60002V10.4C3.10019 10.6105 3.15574 10.8171 3.26105 10.9993C3.36636 11.1815 3.51773 11.3328 3.69998 11.438L7.89998 13.838C8.0824 13.9433 8.28933 13.9988 8.49998 13.9988C8.71062 13.9988 8.91755 13.9433 9.09998 13.838L13.3 11.438C13.4822 11.3328 13.6336 11.1815 13.7389 10.9993C13.8442 10.8171 13.8998 10.6105 13.9 10.4Z"
+                    stroke="#0080FF"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M13.9 8L11.2 9.56V12.674"
+                    stroke="#0080FF"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M5.79998 12.674V9.56L3.09998 8"
+                    stroke="#0080FF"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M3.26196 4.97601L8.49996 8.00601L13.738 4.97601"
+                    stroke="#0080FF"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M8.5 14.048V8"
+                    stroke="#0080FF"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M5.80005 3.32602L8.50005 4.88602L11.2 3.32602"
+                    stroke="#0080FF"
+                    stroke-width="1.2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
                 Tendly Automation is now live
                 <ArrowRight className="size-4 -ml-1" />
               </div>
